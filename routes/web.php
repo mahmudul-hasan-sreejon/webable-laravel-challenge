a031b5ed8804a187aedf6122855ab3994e0b3222
Mahmudul Hasan Sreejon <mahmudul-hasan-sreejon@users.noreply.github.com>
2020-06-11 14:01:59 +0600

    Redirect '/' and '/home' route to '/films' route

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,11 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect("/films");
 });
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', function () {
+    return redirect("/films");
+})->name('home');
